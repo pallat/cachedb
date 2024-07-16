642ad5ae557fd362129a9436956f9ecd26d3e9be
Pallat Anchaleechamaikorn <pallat@Pallats-MacBook-Air.local>
2024-07-16 17:07:36 +0700

    add insert record script, fix redis-insight port

--- postgres/docker-entrypoint-initdb.d/create_table.sql
+++ postgres/docker-entrypoint-initdb.d/create_table.sql
@@ -3,3 +3,7 @@ CREATE TABLE products (
     product_name VARCHAR (50),
     retail_price  NUMERIC(5,2)  
 );
+
+INSERT INTO products(product_name, retail_price) VALUES ('LEATHER BELT',  '12.25');
+INSERT INTO products(product_name, retail_price) VALUES ('WINTER JACKET',  '89.65');
+INSERT INTO products(product_name, retail_price) VALUES ('COTTON SOCKS',  '2.85');
